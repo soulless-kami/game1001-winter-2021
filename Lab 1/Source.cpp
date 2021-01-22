7d0f619debef3149f5a115b6cbf650ecbf68b55e
Damian <Damian.ramkissoon@georgebrown.ca>
2021-01-21 23:08:06 -0500

    Lab 2 Completed

--- Lab 1/Source.cpp
+++ Lab 1/Source.cpp
@@ -1,12 +1,37 @@
+// Simple C++ program
 #include <iostream>
-using namespace std;
+using namespace std;    //use standard namespace 
 
-//this is a comment
+//main entry point to app
 int main()
 {
-	int x - 5; 
+	double first = 2,
+		second = 4,
+		third = 6,
+		fourth = 8,
+		answerAddition,
+		answerSubtraction,
+		answerMulitiplication,
+		answerDivision,
+		answerModulous;
 
-	cout << x;
+	answerAddition = first + second;
+
+
+
+
+	cout << "addiction " << answerAddition << endl;
+	cout << "substraction: " << second - third << endl;
+	cout << "Multiplication " << third * fourth << endl; 
+	cout << "Division " << first / fourth << endl; 
+	cout << "modulous " << (int)first % (int)second << endl; 
+
+
+
+
+ 
+
+
 
 	return 0;
 }
